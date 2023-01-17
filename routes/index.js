72d23538e7eff088f023e8ebd3e534b4fd5e945b
ALEXGAMADEV\alexg <alexgama.97@outlook.com>
2023-01-17 23:00:49 +0000

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages });
+});
+
+/* GET home page. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Create New Message' });
+});
+
+router.post('/new', function(req, res, next) {
+  const { user, message } = req.body;
+  messages.push({
+    text: message,
+    user,
+    added: new Date()
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
